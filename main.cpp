e00d481ca9e3450c93f0f551af2681b485a2231c
Clancy Rowley <cwrowley@princeton.edu>
2023-12-19 11:41:56 -0500

    Add handling for MIDI NoteOn and CC 1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "daisy_pod.h"
+#include "daisysp.h"
 #include "leaflet.h"
 #include "StiffString.h"
 
@@ -12,19 +13,44 @@ StiffString string(sample_rate, NUM_MODES);
 
 volatile float _knob = 0.f;
 
-void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size) {
-  hw.ProcessAllControls();
-  if (hw.button1.RisingEdge()) {
-    string.SetInitialAmplitudes();
-  }
-  float knob2 = hw.GetKnobValue(hw.KNOB_2);
-  string.set_stiffness(0.001f + knob2 * 0.1f);
-  string.set_freq(_knob * 330.0f + 110.0f);
+void AudioCallback(AudioHandle::InputBuffer in,
+                   AudioHandle::OutputBuffer out,
+                   size_t size) {
   for (size_t i = 0; i < size; i++) {
     float sample = string.Tick();
     out[0][i] = sample;
     out[1][i] = sample;
   }
+}
+
+void HandleMidiMessage(MidiEvent m) {
+  switch (m.type) {
+  case NoteOn:
+    {
+      NoteOnEvent p = m.AsNoteOn();
+      if (m.data[1] != 0) {
+        p = m.AsNoteOn();
+        string.set_freq(daisysp::mtof(p.note));
+        string.SetInitialAmplitudes();
+        // string.set_amplitude(p.velocity / 127.0f);
+      }
+    }
+    break;
+  case NoteOff:
+    break;
+  case ControlChange:
+    {
+      ControlChangeEvent p = m.AsControlChange();
+      switch(p.control_number) {
+      case 1:
+        string.set_stiffness(0.2f * ((float) p.value / 127.0f));
+        break;
+      default: break;
+      }
+    }
+  default: break;
+  }
+
 }
 
 int main(void) {
@@ -35,7 +61,12 @@ int main(void) {
   float sample_rate = 48000.f;
   string.Init(sample_rate, NUM_MODES);
   hw.StartAudio(AudioCallback);
+  hw.midi.StartReceive();
   while(1) {
+    hw.midi.Listen();
+    while (hw.midi.HasEvents()) {
+      HandleMidiMessage(hw.midi.PopEvent());
+    }
     _knob = hw.GetKnobValue(hw.KNOB_1);
   }
 }
