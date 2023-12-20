b35ce0c5ae631ecada97ad42d2dfce2837217f91
Clancy Rowley <cwrowley@princeton.edu>
2023-12-20 11:35:52 -0500

    Add velocity sensitivity, and other MIDI parameters
    MIDI parameters:
    CC 1: stiffness
    CC 2: pluck position
    CC 3: high frequency decay
    CC 4: decay

--- main.cpp
+++ main.cpp
@@ -11,12 +11,18 @@
 #include "daisy_pod.h"
 #include "StiffString.h"
 
-const int NUM_MODES = 60;
 
 daisy::DaisyPod hw;
 StiffString string;
 
-volatile float _knob = 0.f;
+const int NUM_MODES = 60;
+const float NOTE_OFF_DECAY = 0.01;
+
+volatile float _knob = 0.0f;
+float amplitude = 0.0f;
+float decay = 0.0f;
+float decay_high_freq = 0.0f;
+int current_note = 0;
 
 inline float midi_to_freq(float m) {
   // Convert a MIDI note to frequency in Hz
@@ -27,31 +33,51 @@ void AudioCallback(daisy::AudioHandle::InputBuffer in,
                    daisy::AudioHandle::OutputBuffer out,
                    size_t size) {
   for (size_t i = 0; i < size; i++) {
-    float sample = string.Tick();
+    float sample = amplitude * string.Tick();
     out[0][i] = sample;
     out[1][i] = sample;
   }
 }
 
+inline float MidiScale(int midi_value, float min, float max) {
+  // Scale a MIDI parameter value (between 0 and 127) to the range [min, max]
+  return min + static_cast<float>(midi_value) / 127.0f * (max - min);
+}
+
 void HandleMidiMessage(daisy::MidiEvent m) {
   switch (m.type) {
     case daisy::NoteOn: {
-      daisy::NoteOnEvent p = m.AsNoteOn();
-      if (m.data[1] != 0) {
-        p = m.AsNoteOn();
-        string.set_freq(midi_to_freq(p.note));
-        string.SetInitialAmplitudes();
-        // string.set_amplitude(p.velocity / 127.0f);
+      auto p = m.AsNoteOn();
+      current_note = p.note;
+      string.set_freq(midi_to_freq(p.note));
+      string.set_decay(decay);
+      string.SetInitialAmplitudes();
+      amplitude = MidiScale(p.velocity, 0.0f, 1.0f);
+    }
+      break;
+    case daisy::NoteOff: {
+      auto p = m.AsNoteOff();
+      if (p.note == current_note) {
+        string.set_decay(NOTE_OFF_DECAY);
       }
     }
       break;
-    case daisy::NoteOff:
-      break;
     case daisy::ControlChange: {
-      daisy::ControlChangeEvent p = m.AsControlChange();
+      auto p = m.AsControlChange();
       switch (p.control_number) {
         case 1:
-          string.set_stiffness(0.2f * (static_cast<float>(p.value / 127.0f)));
+          string.set_stiffness(MidiScale(p.value, 0.0f, 0.2f));
+          break;
+        case 2:
+          string.set_pluck_pos(MidiScale(p.value, 0.001f, 1.0f));
+          break;
+        case 3:
+          decay_high_freq = MidiScale(p.value, 0.0f, 0.0005f);
+          string.set_decay_high_freq(decay_high_freq);
+          break;
+        case 4:
+          decay = MidiScale(p.value, 0.0f, 0.005f);
+          string.set_decay(decay);
           break;
         default: break;
       }
